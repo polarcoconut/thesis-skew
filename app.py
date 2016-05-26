82f7e0a191957d5a18cd1829417ddf5cecd85ec7
Christopher Lin <christopher.h.lin@gmail.com>
2016-05-26 16:44:22 -0700

    no compile errors

--- app.py
+++ app.py
@@ -4,15 +4,26 @@ from flask.ext.cors import CORS
 from flask.ext.restful import Api
 from flask.ext.mail import Mail
 import uuid
-#import redis
-#from rq import Worker, Queue, Connection
 from rq import Queue
+import redis
+from boto.mturk.connection import MTurkConnection
 
 app = Flask(__name__)
 
 app.config.from_object(os.environ['APP_SETTINGS'])
-#app.redis = redis.StrictRedis.from_url(app.config['REDIS_URL'])
+app.AWS_ACCESS_KEY_ID = app.config['AWS_ACCESS_KEY_ID']
+app.AWS_SECRET_ACCESS_KEY = app.config['AWS_SECRET_ACCESS_KEY']
 
+app.CROWDJS_API_KEY = app.config['CROWDJS_API_KEY']
+app.CROWDJS_GET_ANSWERS_URL = app.config['CROWDJS_GET_ANSWERS_URL']
+app.CROWDJS_REQUESTER_ID = app.config['CROWDJS_REQUESTER_ID']
+
+app.MTURK_HOST = app.config['MTURK_HOST']
+app.CONTROLLER = app.config['CONTROLLER']
+app.CONTROLLER_BATCH_SIZE = app.config['CONTROLLER_BATCH_SIZE']
+app.CONTROLLER_APQ = app.config['CONTROLLER_APQ']
+
+app.EXAMPLE_CATEGORIES = app.config['EXAMPLE_CATEGORIES']
 
 api = Api(app)
 
@@ -25,10 +36,16 @@ print "Loading mail extension"
 sys.stdout.flush()
 mail = Mail(app)
 
-print "Loading redis queue"
+print "Loading redis and redis queue"
 from worker import conn
 app.rq = Queue(connection = conn)
-    
+app.redis = redis.StrictRedis.from_url(app.config['REDIS_URL'])
+
+print "Setting up Mturk connection"
+app.mturk = MTurkConnection(app.AWS_ACCESS_KEY_ID,
+                            app.AWS_SECRET_ACCESS_KEY,
+                            host=app.MTURK_HOST)
+
 print "Loading logging"
 sys.stdout.flush()
 if not app.debug:   
@@ -43,8 +60,24 @@ sys.stdout.flush()
 
 @app.route('/')
 def hello():
-    return "Welcome to Extremest Extraction v0.01!"
-    
+    html = "<p>Welcome to Extremest Extraction v0.01!</p>"
+    html += '<form action="" method="post">Event: <input type="text"/><br/>'
+    html += 'Event Definition: <input type="text"/></br/>'
+    html += 'Event Good Example 1: <input type="text"/></br/>'
+    html += 'Event Good Example 1 Trigger: <input type="text"/></br/>'
+    html += 'Event Good Example 2: <input type="text"/></br/>'
+    html += 'Event Good Example 2 Trigger: <input type="text"/></br/>'
+    html += 'Event Bad Example 1: <input type="text"/></br/>'
+    html += 'Event Negatve Good Example 1: <input type="text"/></br/>'
+    html += 'Event Negatve Bad Example 1: <input type="text"/></br/>'
+    html += '<input type="submit" value="Submit"></form>'
+    return html
+
+
+
 
 from api.taboo_api import *
 api.add_resource(ComputeTabooApi, '/taboo') 
+
+from api.train_api import *
+api.add_resource(TrainExtractorApi, '/train')
--- config.py
+++ config.py
@@ -24,13 +24,48 @@ class Config(object):
     SECURITY_PASSWORD_SALT = 'abcde'
 
     MAIL_SUPPRESS_SEND = True
+
+    AWS_ACCESS_KEY_ID = 'AKIAIOGNWDMLQIYOKRQQ'
+    AWS_SECRET_ACCESS_KEY = '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'
+
+
+    #CROWDJS STUFF
+    CROWDJS_API_KEY = 'seomthing'
+    CROWDJS_REQUESTER_ID = 'asdfasdfasdf'
+    CROWDJS_GET_ANSWERS_URL = 'http://crowdjs.heroku.com/answers?'
+    CROWDJS_PUT_TASK_URL =  'http://crowdjs.heroku.com/tasks'
+    CROWDJS_PUT_QUESTIONS_URL =  'http://crowdjs.heroku.com/questions'
+
+
+
+    
+    PRECISION_EXAMPLE_TASK = {'hit_layout_id' : 'alskdfadf',
+                              'needs_data': True,
+                              'task_name' : 'Event Negation',
+                              'task_description' : 'Negate a sentence'}
+    RECALL_EXAMPLE_TASK = {'hit_layout_id' : 'aklsjdlka',
+                           'needs_data' : False,
+                           'task_name' : 'Event Generation',
+                           'task_description' : 'Generate sentences'}
+
+    
+    EXAMPLE_CATEGORIES = [RECALL_EXAMPLE_TASK, PRECISION_EXAMPLE_TASK]
+
+
+    #greedy, fixed
+    CONTROLLER = 'greedy'
+    CONTROLLER_BATCH_SIZE = 100
+    CONTROLLER_APQ = 2
+
     
 class DevelopmentConfig(Config):
     DEVELOPMENT = True
     DEBUG = True
     TESTING = True
+    MTURK_HOST = 'mechanicalturk.sandbox.amazonaws.com'
 
 class Production(Config):
     DEBUG = False
     DEVELOPMENT = False
     TESTING = False
+    MTURK_HOST = 'mechanicalturk.amazonaws.com'
