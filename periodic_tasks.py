6feaff150b960617977680ae48f54fd9d30e81b5
Christopher Lin <christopher.h.lin@gmail.com>
2016-09-05 13:42:56 -0700

    made a better distributed lock

--- periodic_tasks.py
+++ periodic_tasks.py
@@ -2,6 +2,7 @@ from api.train import restart
 from app import app
 from schema.job import Job
 import sys, os, traceback
+import redis
 
 @app.celery.task(name='run_gather')
 def run_gather():
@@ -9,38 +10,38 @@ def run_gather():
     jobs_checked = 0
     jobs = Job.objects(status='Running')
 
-    
     for job in jobs:
         #FOR DEBUGGING PURPOSES
         #job.lock = False
         #job.save()
         #raise Exception
 
-        try:
-            if job.lock:
-                print"job is locked. go to next job"
-                continue
-        except AttributeError:
-            print "job doesn't have lock property"
-
         if len(job.checkpoints.keys()) == 0:
             continue
-        jobs_checked += 1
-        print "Running Gather for job %s" % job.id
-        job.lock = True
-        job.save()
-        try:
-            restart(job.id)
-        except Exception as e:
-            print "Exception:"
-            print '-'*60
-            traceback.print_exc(file=sys.stdout)
-            print '-'*60
-            print e
-            
-        job.lock = False
-        job.save()
+
+        lock_key = job.id
+        redis_handle = redis.Redis.from_url(app.config['REDIS_URL'])
         
+        acquire_lock = lambda: redis_handle.setnx(lock_key, '1')
+        release_lock = lambda: redis_handle.delete(lock_key)
+
+        if acquire_lock():
+            try:
+                jobs_checked += 1
+                print "Running Gather for job %s" % job.id
+                restart(job.id)                
+            except Exception:
+                print "Exception:"
+                print '-'*60
+                traceback.print_exc(file=sys.stdout)
+                print '-'*60
+                print e
+            finally:
+                release_lock()
+                        
+        else:
+            print"job is locked. go to next job"
+                
     print "%d jobs restarted" % jobs_checked
     
     return True
