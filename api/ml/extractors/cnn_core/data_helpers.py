c647bb210bd688790ba0dec0d83e428af6613390
Christopher Lin <christopher.h.lin@gmail.com>
2016-09-28 13:57:21 -0700

    corner case for num_batches

--- api/ml/extractors/cnn_core/data_helpers.py
+++ api/ml/extractors/cnn_core/data_helpers.py
@@ -222,7 +222,9 @@ def batch_iter(data, batch_size, num_epochs, shuffle=True):
     """
     data = np.array(data)
     data_size = len(data)
-    num_batches_per_epoch = int(len(data)/batch_size) + 1
+    num_batches_per_epoch = float(len(data))/batch_size
+    if not num_batches_per_epoch.is_integer():
+        num_batches_per_epoch = int(num_batches_per_epoch) + 1
     for epoch in range(num_epochs):
         # Shuffle the data at each epoch
         if shuffle:
